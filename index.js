e92079794504d89bb61a34ce84c28cf6a990ae40
0xflotus <0xflotus@gmail.com>
2018-11-17 23:54:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const MongoClient = require("mongodb").MongoClient;
 MongoClient.connect(
   `mongodb://${process.env["USER_NAME"]}:${
     process.env["USER_PW"]
-  }@${process.env["HOST"]}:${process.env["PORT"]}/?authMechanism=DEFAULT&authSource=beverages`,
+  }@${process.env["HOST"]}:${process.env["PORT"]}/?authMechanism=DEFAULT&authSource=${process.env["COLL_NAME"]}`,
   { useNewUrlParser: true },
   function(err, client) {
     client
